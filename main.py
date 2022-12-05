46e013c6221161c41aa6862d7038745c4ff8a45f
theguyfromratatouille <sillyjunju@gmail.com>
2022-12-04 18:36:10 -0800

    camera settings

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from picamera import PiCamera 
 from time import sleep 
 
-camera = PiCamera()
-camera.rotation = 180
-
 while true: 
-camera.start_preview()
-sleep(5) 
-camera.stop_preview()
+    camera = PiCamera()
+    camera.rotation = 180
+    camera.resolution = (1024,600)
+    camera.framerate = 24
+    camera.exposure_mode = auto 
+    camera.awb_mode = auto 
+    camera.brightness = 50 
+    #camera.image_effect = denoise
+
+
+
+    camera.start_recording()
+    camera.wait_recording(20)
+camera.stop_recording
+
+
+
+
+ 
